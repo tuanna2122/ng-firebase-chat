6bd35ae11521c617300a14dd2b89f313db612684
Tuan Nguyen Anh <tuanna2122@gmail.com>
2017-09-07 17:58:06 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire4/database';
+import { AngularFireAuth } from 'angularfire4/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items: FirebaseListObservable<any[]>;
+  user: Observable<firebase.User>;
+  msgVal: string = '';
+
+  displayName: string = '';
+
+  constructor(public afDb: AngularFireDatabase, public afAuth: AngularFireAuth) {
+    this.items = this.afDb.list('/messages', {
+      query: {
+        limitToLast: 5
+      }
+    });
+
+    this.user = this.afAuth.authState;
+    this.user.subscribe(user => {
+      if (user.uid) {
+        this.displayName = user.displayName;
+      }
+    });
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  }
+
+  chatSend(theirMessage: string) {
+    this.items.push({
+      message: theirMessage,
+      name: this.displayName
+    });
+    this.msgVal = '';
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire4';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire4/database';
+import { AngularFireAuthModule } from 'angularfire4/auth';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA5KYIlmQwZoC1qmbWBfNDx_XDoY4mB7Sc",
+  authDomain: "mychatapp-6de6f.firebaseapp.com",
+  databaseURL: "https://mychatapp-6de6f.firebaseio.com",
+  //projectId: "mychatapp-6de6f",
+  storageBucket: "",
+  messagingSenderId: "396722560155"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
